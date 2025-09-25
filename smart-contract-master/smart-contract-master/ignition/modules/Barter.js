63bea60b9c2c4415360b81cee5446f2df781cc21
Barter <68006590+bartersmartplace@users.noreply.github.com>
2025-09-25 17:11:38 +0300

    Delete smart-contract-master/smart-contract-master directory

--- smart-contract-master/smart-contract-master/ignition/modules/Barter.js
+++
@@ -1,7 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("BarterModule", (m) => {
-  const barter = m.contract("Barter", ["Barter", "BRTR"]);
-
-  return { barter };
-});
--- smart-contract-master/smart-contract-master/ignition/modules/ERC20.js
+++
@@ -1,7 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("ERC20Module", (m) => {
-  const erc20 = m.contract("ERC20", ["Barter", "BRTR"]);
-
-  return { erc20 };
-});
